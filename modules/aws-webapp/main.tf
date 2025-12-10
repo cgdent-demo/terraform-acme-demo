95c520b9e995f25e78a602b97b949971a3e66864
Craig Dent <cgdent@yahoo.com>
2025-12-10 12:17:18 +1100

    Update main.tf
    removed variables and outputs from main.tf

--- modules/aws-webapp/main.tf
+++ modules/aws-webapp/main.tf
@@ -1,8 +1,3 @@
-variable "name" {}
-variable "vpc_cidr" { default = "10.0.0.0/16" }
-variable "public_subnet_cidr" { default = "10.0.1.0/24" }
-variable "tags" { type = map(string) }
-
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
   tags       = var.tags
@@ -14,6 +9,3 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch  = true
   tags                     = var.tags
 }
-
-output "vpc_id" { value = aws_vpc.this.id }
-output "public_subnet_id" { value = aws_subnet.public.id }
