bd9d87b7b36084adcae893e30030b75065fd0980
Craig Dent <cgdent@yahoo.com>
2025-12-12 16:01:36 +1100

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -1,7 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
-  required_providers {
-    azurerm = { source = "hashicorp/azurerm" version = "~> 4.0" }
+
+  required_providers = {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
   }
 }
 
